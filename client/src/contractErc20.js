4bf67b960ac166d07c4ac44d4f8b0e145ea4ab75
nik379 <nikhilgupta3501@gmail.com>
2024-02-26 02:37:56 +0530

    Updated contractErc20.js

--- client/src/contractErc20.js
+++ client/src/contractErc20.js
@@ -7,8 +7,9 @@ const erc20 = new ERC20();
 // define the main function
 const main = async () => {
     // call the deployContract method
-    const result = await erc20.deployContract('klay','e04f31664c36bfd191b8884bad043fbed0f9da89f9d37c1ab38603bc77e5cd5b',5000,"finalTEST","TEST");
-
+    const result = await erc20.deployContract('klay','e04f31664c36bfd191b8884bad043fbed0f9da89f9d37c1ab38603bc77e5cd5b',5000,"cofToken","COF");
+    //parameters of deployContract function are in this this order: chain,private-key,no.of tokens,token name,token symbol
+    
     // print the receipt
     console.log(`Contract deployed at ${result.contract.address}`);
 };
